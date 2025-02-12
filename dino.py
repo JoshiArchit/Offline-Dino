0b5dfa322a838b3d66c15ad290a895ac0b27bba8
Archit Joshi <joshi.archit.2904@gmail.com>
2025-02-11 21:33:51 -0600

    Initial commit

---
+++ dino.py
@@ -0,0 +1,7 @@
+"""
+Filename : dino.py
+Author : Archit
+Date Created : 2/11/2025
+Description : 
+Language : python3
+"""
---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Filename : window.py
+Author : Archit
+Date Created : 2/11/2025
+Description : 
+Language : python3
+"""
+import pygame as pg
